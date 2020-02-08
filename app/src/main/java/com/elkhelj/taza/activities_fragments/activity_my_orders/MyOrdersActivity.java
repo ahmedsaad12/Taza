aa0d1b3b1b2b9669c3393c965e302de960001a62
End Point <ahmedmohamed23345@gmail.com>
2020-02-08 02:15:19 -0800

    ch

--- app/src/main/java/com/elkhelj/taza/activities_fragments/activity_my_orders/MyOrdersActivity.java
+++ app/src/main/java/com/elkhelj/taza/activities_fragments/activity_my_orders/MyOrdersActivity.java
@@ -10,11 +10,15 @@ import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
 import androidx.databinding.DataBindingUtil;
+import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
 import com.elkhelj.taza.R;
+import com.elkhelj.taza.activities_fragments.activity_my_orders.fragments.Fragment_Current_Order;
+import com.elkhelj.taza.activities_fragments.activity_my_orders.fragments.Fragment_Finshied_Order;
 import com.elkhelj.taza.adapters.Order_Adapter;
+import com.elkhelj.taza.adapters.ViewPagerAdapter;
 import com.elkhelj.taza.databinding.ActivityMyOrdersBinding;
 import com.elkhelj.taza.interfaces.Listeners;
 import com.elkhelj.taza.language.LanguageHelper;
@@ -39,17 +43,13 @@ import retrofit2.Response;
 public class MyOrdersActivity extends AppCompatActivity implements Listeners.BackListener {
     private ActivityMyOrdersBinding binding;
     private String lang;
-    private Order_Adapter ads_adapter;
-    private List<Order_Model> advesriment_data_list;
-    private LinearLayoutManager manager;
-    private boolean isLoading = false;
-    private int current_page2 = 1;
-    private Preferences preferences;
-    private UserModel userModel;
+    private List<Fragment> fragmentList;
+    private List<String> titles;
+    private ViewPagerAdapter adapter;
     @Override
     protected void attachBaseContext(Context newBase) {
         Paper.init(newBase);
-        super.attachBaseContext(LanguageHelper.updateResources(newBase, Paper.book().read("lang", "en")));
+        super.attachBaseContext(LanguageHelper.updateResources(newBase, Paper.book().read("lang", Locale.getDefault().getLanguage())));
 
     }
 
@@ -58,109 +58,45 @@ public class MyOrdersActivity extends AppCompatActivity implements Listeners.Bac
         super.onCreate(savedInstanceState);
         binding = DataBindingUtil.setContentView(this, R.layout.activity_my_orders);
         initView();
-        if(userModel!=null){
-        getAds();
-            }
+
 
     }
 
     private void initView() {
-        advesriment_data_list=new ArrayList<>();
-        preferences= Preferences.newInstance();
-        userModel=preferences.getUserData(this);
+
         Paper.init(this);
         lang = Paper.book().read("lang", Locale.getDefault().getLanguage());
         binding.setLang(lang);
+        fragmentList = new ArrayList<>();
+        titles = new ArrayList<>();
         binding.setBackListener(this);
-        manager = new LinearLayoutManager(this);
+        binding.tab.setupWithViewPager(binding.pager);
+        addFragments_Titles();
+        binding.pager.setOffscreenPageLimit(fragmentList.size());
 
-        binding.progBar.getIndeterminateDrawable().setColorFilter(ContextCompat.getColor(this, R.color.colorPrimary), PorterDuff.Mode.SRC_IN);
-        binding.recView.setLayoutManager(manager);
-        ads_adapter = new Order_Adapter(advesriment_data_list,this);
-        binding.recView.setItemViewCacheSize(25);
-        binding.recView.setDrawingCacheQuality(View.DRAWING_CACHE_QUALITY_HIGH);
-        binding.recView.setDrawingCacheEnabled(true);
-        binding.progBar.setVisibility(View.GONE);
-        binding.llNoStore.setVisibility(View.GONE);
-        binding.swipeRefresh.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
-            @Override
-            public void onRefresh() {
-                getAds();
-            }
-        });
+        adapter = new ViewPagerAdapter(getSupportFragmentManager());
+        adapter.addFragments(fragmentList);
+        adapter.addTitles(titles);
+        binding.pager.setAdapter(adapter);
 
-        //binding.recView.setAdapter(ads_adapter);
+
+
+
+
+
 
     }
-    public void getAds() {
-        //   Common.CloseKeyBoard(homeActivity, edt_name);
-        advesriment_data_list.clear();
-        ads_adapter.notifyDataSetChanged();
-        binding.progBar.setVisibility(View.VISIBLE);
 
-        // rec_sent.setVisibility(View.GONE);
-        try {
+    private void addFragments_Titles() {
+        fragmentList.add(Fragment_Current_Order.newInstance());
+        fragmentList.add(Fragment_Finshied_Order.newInstance());
 
 
-            Api.getService( Tags.base_url)
-                    .getMyAds(userModel.getId()+"",userModel.getType()+"")
-                    .enqueue(new Callback<List<Order_Model>>() {
-                        @Override
-                        public void onResponse(Call<List<Order_Model>> call, Response<List<Order_Model>> response) {
-                            binding.swipeRefresh.setRefreshing(false);
-
-                            binding.progBar.setVisibility(View.GONE);
-                            if (response.isSuccessful() && response.body() != null && response.body() != null) {
-                                advesriment_data_list.clear();
-                                advesriment_data_list.addAll(response.body());
-                                if (response.body().size() > 0) {
-                                    // rec_sent.setVisibility(View.VISIBLE);
-                                    //  Log.e("data",response.body().getData().get(0).getAr_title());
-
-                                    binding.llNoStore.setVisibility(View.GONE);
-                                    ads_adapter.notifyDataSetChanged();
-                                    //   total_page = response.body().getMeta().getLast_page();
-
-                                } else {
-                                    ads_adapter.notifyDataSetChanged();
-
-                                    binding.llNoStore.setVisibility(View.VISIBLE);
-
-                                }
-                            } else {
-                                ads_adapter.notifyDataSetChanged();
-
-                                binding.llNoStore.setVisibility(View.VISIBLE);
-
-                                //Toast.makeText(activity, getString(R.string.failed), Toast.LENGTH_SHORT).show();
-                                try {
-                                    Log.e("Error_code", response.code() + "_" + response.errorBody().string());
-                                } catch (IOException e) {
-                                    e.printStackTrace();
-                                }
-                            }
-                        }
-
-                        @Override
-                        public void onFailure(Call<List<Order_Model>> call, Throwable t) {
-                            try {
-                                binding.swipeRefresh.setRefreshing(false);
-
-                                binding.progBar.setVisibility(View.GONE);
-                                binding.llNoStore.setVisibility(View.VISIBLE);
+        titles.add(getString(R.string.current_order));
+        titles.add(getString(R.string.finish_order));
 
 
-                           //     Toast.makeText(MyOrdersActivity.this, getString(R.string.something), Toast.LENGTH_SHORT).show();
-                                Log.e("error", t.getMessage());
-                            } catch (Exception e) {
-                            }
-                        }
-                    });
-        }catch (Exception e){
-            binding.progBar.setVisibility(View.GONE);
-            binding.llNoStore.setVisibility(View.VISIBLE);
 
-        }
     }
 
 
